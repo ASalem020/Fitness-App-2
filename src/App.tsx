f79ddbdaeea934f44036ab20df168c8f058ef27a
Michael Samy <michaelsamy196@gmail.com>
2026-03-16 21:51:42 +0200

    feat(footer): add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Footer from "./components/shared/footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -114,8 +115,10 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
