44b8a1f84b9638607dbc106f386db535598e646d
Yassa <yassi@example.com>
2026-03-17 17:57:05 +0200

    feat(meals-section): add MealsSection and MealCard components with images

---
+++ src/components/meals-section/meals-card.tsx
@@ -0,0 +1,31 @@
+import { ArrowRight } from "lucide-react";
+
+type prpos = {
+  title: string;
+  image: string;
+};
+
+export default function MealCard({ title, image }: prpos) {
+  return (
+    <div className=" w-96 h-96 group rounded-2xl overflow-hidden cursor-pointer shadow-md hover:shadow-xl transition">
+      {/* Image */}
+      <img
+        src={image}
+        alt={title}
+        className="w-full h-[300px] object-cover transition-transform duration-500 group-hover:scale-110"
+      />
+
+      {/* Content */}
+      <div className=" p-3 bg-gray-300 text-black flex flex-col justify-start items-start gap-2">
+        <h3 className=" uppercase text-xl font-bold start-1">{title}</h3>
+
+        <button className="flex items-center gap-2 mt-2 text-sm font-semibold text-orange-600 group-hover:gap-3 transition-all">
+          Read More
+          <span className="bg-orange-600 text-black rounded-full p-1">
+            <ArrowRight size={14} />
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+}
