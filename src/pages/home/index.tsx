7a9f42b482bd91204012421e29d5a80f2a26f313
Michael Samy <michaelsamy196@gmail.com>
2026-03-20 20:06:20 +0200

    feat(workouts): add section in home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import WorkoutsSection from "./sections/workouts/components/workouts-section";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <WorkoutsSection />
+    </div>
+  );
+}
