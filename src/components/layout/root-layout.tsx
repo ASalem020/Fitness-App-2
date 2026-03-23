528724f8f56007c79481963d153bf9228cf767c9
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-03-23 18:23:25 +0200

    feat(why-us-section): create section title using svg & text tags in light & dark mode

--- src/components/layout/root-layout.tsx
+++ src/components/layout/root-layout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../shared/header";
 import Footer from "../shared/footer";
+import { useTheme } from "@/hooks/use-theme";
 
 export default function RootLayout() {
+  // Hooks
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <>
       {/* Header */}
@@ -15,6 +19,14 @@ export default function RootLayout() {
 
       {/* Footer */}
       <Footer />
+
+      {/* This Button for test dark mode only */}
+      <button
+        onClick={toggleTheme}
+        className="fixed bottom-9 right-9 rounded-full size-12 bg-slate-800 dark:bg-slate-400 text-slate-100 dark:text-slate-800 hover:scale-110 transition-transform duration-200 flex items-center justify-center font-semibold"
+      >
+        {theme === "light" ? "☀️" : "🌙"}
+      </button>
     </>
   );
 }
--- src/components/shared/section-title.tsx
+++ src/components/shared/section-title.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@/hooks/use-theme";
 import { Dumbbell } from "lucide-react";
 
 type SectionTitleProps = {
@@ -5,19 +6,42 @@ type SectionTitleProps = {
 };
 
 export default function SectionTitle({ title }: SectionTitleProps) {
+  // Hooks
+  const { theme } = useTheme();
+
   return (
     <div className="section-title font-baloo-thambi relative">
       {/* Styled Title */}
-      <h2
-        style={
-          {
-            WebkitTextStroke: "2px #BCBDBF",
-          } as React.CSSProperties
-        }
-        className="text-white dark:text-zinc-800 text-6xl hidden md:block w-fit uppercase font-bold relative after:absolute after:size-full after:bg-gradient-to-b after:from-white/0 after:to-80% after:to-white after:z-10 after:inset-0"
-      >
-        {title}
-      </h2>
+      <div className="decorated-text relative after:absolute after:bg-gradient-to-b after:from-transparent after:via-45% after:via-white/25 after:to-75% after:to-white dark:after:via-zinc-800/25 dark:after:to-zinc-800 after:w-[15.625rem] after:h-[4.375rem] after:inset-0 w-fit">
+        <svg width="250" height="70" viewBox="0 0 250 70">
+          <text
+            x="45%"
+            y="60%"
+            dominantBaseline="middle"
+            textAnchor="middle"
+            fontFamily="Baloo Thambi 2, sans-serif"
+            fontSize="64px"
+            fontWeight="bold"
+            fill="none"
+            stroke={theme === "light" ? "#DBDCDC" : "#535454"}
+            strokeWidth="3"
+          >
+            {title.toUpperCase()}
+          </text>
+          <text
+            x="45%"
+            y="60%"
+            dominantBaseline="middle"
+            textAnchor="middle"
+            fontFamily="Baloo Thambi 2, sans-serif"
+            fontSize="64px"
+            fontWeight="bold"
+            fill={theme === "light" ? "#ffffff" : "#27272a"}
+          >
+            {title.toUpperCase()}
+          </text>
+        </svg>
+      </div>
 
       <p className="text-sm text-[#FF4100] flex items-center gap-2 capitalize font-semibold -mt-5 ml-2 relative z-10">
         {/* Icon */}
