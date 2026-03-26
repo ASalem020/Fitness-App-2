645f4b7635faad7ad146e87575b6ae76545a1c39
Michael Samy <michaelsamy196@gmail.com>
2026-03-26 13:57:20 +0200

    add headers for language change

--- src/lib/services/workout.service.ts
+++ src/lib/services/workout.service.ts
@@ -1,7 +1,11 @@
 import type { MusclesGroupResponse, MusclesResponse } from "../types/muscles";
 
 export async function getMusclesGroupsService(): Promise<MusclesGroupResponse> {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/muscles`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/muscles`, {
+    headers: {
+      "accept-language": "en",
+    },
+  });
   if (!response.ok) {
     throw new Error("Failed to fetch muscles");
   }
@@ -11,6 +15,11 @@ export async function getMusclesGroupsService(): Promise<MusclesGroupResponse> {
 export async function getMusclesService(id: string): Promise<MusclesResponse> {
   const response = await fetch(
     `${import.meta.env.VITE_API_URL}/musclesGroup/${id}`,
+    {
+      headers: {
+        "accept-language": "en",
+      },
+    },
   );
 
   if (!response.ok) {
