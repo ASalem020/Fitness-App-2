c4a1a54d170a702c16ba9c0e5783ee8650db3658
Yassa <yassi@example.com>
2026-03-31 01:14:48 +0200

    feat(responsive-about-us-section): a responsive about us section has been added

--- src/pages/about/components/feature.tsx
+++ src/pages/about/components/feature.tsx
@@ -8,11 +8,11 @@ type Props = {
 export default function Feature({ feature }: Props) {
   return (
     <>
-      <div className="mb-16">
+      <div className="mb-8 lg:mb-16">
         <h4 className="font-semibold flex items-center gap-4">
           <img src={feature.img} alt="arrow up icon" /> {feature.title}
         </h4>
-        <p className="text-sm mt-4 w-60">{feature.text}</p>
+        <p className="text-sm mt-4 w-60 ">{feature.text}</p>
       </div>
     </>
   );
