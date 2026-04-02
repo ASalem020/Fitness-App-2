200295cfed4c1dd31330487132d49549df7742a4
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-04-02 17:54:47 +0200

    feat(translation): start setup use-intl for translation

--- src/components/providers/app/index.tsx
+++ src/components/providers/app/index.tsx
@@ -1,10 +1,18 @@
+import { IntlProvider } from "use-intl";
 import TanstackQueryProvider from "./components/tanstack-query.provider";
 import { ThemeProvider } from "./components/theme-provider";
+import { messages } from "@/i18n/messages";
+import { useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  // States
+  const [locale, setLocale] = useState<"en" | "ar">("ar");
+
   return (
-    <ThemeProvider>
-      <TanstackQueryProvider>{children}</TanstackQueryProvider>
-    </ThemeProvider>
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <ThemeProvider>
+        <TanstackQueryProvider>{children}</TanstackQueryProvider>
+      </ThemeProvider>
+    </IntlProvider>
   );
 }
