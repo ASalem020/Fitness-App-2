ded739fa7566de03d12af68c9fcdd74500285ab4
Michael Samy <michaelsamy196@gmail.com>
2026-04-16 14:42:53 +0200

    Merge dev into KYC-steps branch

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -6,7 +6,7 @@ import { Loader } from "lucide-react";
 import { cn } from "@/lib/utils/tailwind-merge";
 
 const buttonVariants = cva(
-  "inline-flex relative items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 font-baloo-thambi rtl:font-tajawal",
+  "inline-flex relative items-center justify-center gap-2 whitespace-nowrap rounded-full text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:bg-zinc-300 disabled:text-white disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
